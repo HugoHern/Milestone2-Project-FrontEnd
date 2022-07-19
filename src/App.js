60e734208d186db9e5a212c510ee03f93f552134
Hugo F Hernandez <97071972+HugoHern@users.noreply.github.com>
2022-07-18 20:47:19 -0400

    Adding starting files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header.js';
+import DatingCards from './components/DatingCards.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DatingCards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DatingCards.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import DatingCard from 'react-tinder-card'
+import './DatingCards.css'
+
+const DatingCards = () => {
+    //state for temporary data of people.
+    const [people, setPeople] = useState([
+        {
+            name: 'Random Guy', 
+            imgUrl: 'boy_pic.jpg'
+        },
+        {
+            name: 'Another Guy',
+            imgUrl: 'boy_pic.jpg'
+        },
+        {
+            name: 'Random Girl',
+            imgUrl: 'girl_pic.jpg'
+        },
+        {
+            name: "Another Girl",
+            imgUrl: 'girl_pic.jpg'
+        }
+    ])
+
+    //helper functions to use with tinder library
+    //directions tells the app which direction the user swiped, and nametodelete will be used to remove from screen
+    const swiped = (direction, nameToDelete) => {
+        console.log('receiving' + nameToDelete)
+    }
+    //keeps track of what person person has left the screen
+    const outOfFrame = (name) => {
+        console.log(name + 'left the screen!')
+    }
+
+    /* map through the state or data from the people object. Passing in helper functions to the Dating Card component for swiping animations with help of react-tinder*/
+    return (
+        <div className="datingCards">
+            <div className="datingCards_container">
+                {people.map((person) => (
+                    <DatingCard className="swipe" key={person.name} preventSwipe={['up', 'down']} onSwipe={(dir) => swiped(dir, person.name)} onCardLeftScreen = {() => outOfFrame(person.name)}>
+                        <div style={{backgroundImage: `url(${person.imgUrl})`}} className='card'>
+                            <h3>{person.name}</h3>
+                        </div>
+                    </DatingCard>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default DatingCards
