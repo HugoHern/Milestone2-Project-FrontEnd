1ac42cae1368978ae967dc5d2cf4203e99dcec72
Jacob Lamb <jacoblamb15@gmail.com>
2022-07-20 21:23:21 -0400

    added react route for Their Profile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // switching to development branch
 import './App.css';
+import { useEffect, useState, Fragment } from 'react';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.js';
 import DatingCards from './components/DatingCards.js'
+import TheirProfile from './components/TheirProfile';
 
 function App() {
+
   return (
     <div className="App">
-      <Header />
-      <DatingCards />
+      <Router>
+        <header>
+          <Header />
+        </header>
+      <Link to ="/TheirProfile">
+        <DatingCards />
+      </Link>
+
+      <Routes>
+        <Route path="/TheirProfile" element = {<TheirProfile />} />
+      </Routes>
+      </Router>
+      
     </div>
   );
 }
