2634ef5514f9222d837633dfa96e4514317abf60
HugoHern <hugo8954@gmail.com>
2022-07-21 22:18:50 -0400

    modifed css styling for video and backgrounds to match with app color theme

--- src/components/DatingCards.js
+++ src/components/DatingCards.js
@@ -45,7 +45,7 @@ const DatingCards = () => {
                                     <iframe width="400" height="400" src="https://www.youtube.com/embed/0bomkgXeDkE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                              
                        
-                            <h3>{person.name}</h3>
+                            <h2>{person.name}</h2>
                         </div>
                     </DatingCard>
                 ))}
